f8ba528df3cabe42204779f3f2de984648eecad4
allanschougaard <allanschougaard@PAD6.attlocal.net>
2022-06-05 18:57:40 -0700

    Remove median challenge

--- CISC191M1Challenges/src/edu/sdmesa/cisc191/TestM1Challenge.java
+++ CISC191M1Challenges/src/edu/sdmesa/cisc191/TestM1Challenge.java
@@ -116,6 +116,8 @@ class TestM1Challenge
 //		assertEquals(1, getMiddle(array1));
 //		int[] array2 = { 0, 4, 8 };
 //		assertEquals(4, getMiddle(array2));
+//	    int[] array3 = { 0, 1, 2, 3, 4 };
+//	    assertEquals(2, getMiddle(array3));
 //	}
 //	
 //	@Test
@@ -189,29 +191,5 @@ class TestM1Challenge
 //		assertEquals((-1+(-2)+(-3))/3.0, average(array4));
 //	}
 //
-//	@Test
-//	void testMedian()
-//	{
-//      // The median of a set of numbers is the "middle" number,
-//      // or the average of the two middle numbers.
-//      // See: https://en.wikipedia.org/wiki/Median
-//      // Assume: the input array is sorted
-//      // Hint: a number is even if: number % 2 == 0
-//      // Hint: one middle element has index: length / 2
-//
-//		int[] array1 = {17};
-//		assertEquals(17.0, median(array1));
-//		
-//		int[] array2 = {1, 2, 5};
-//		assertEquals(2.0, median(array2));
-//
-//		int[] array3 = {1, 3};
-//		assertEquals((1+3)/2.0, median(array3));
-//
-//		int[] array4 = {1, 3, 3, 6, 7, 8, 9};
-//		assertEquals(6.0, median(array4));
-//	
-//	    int[] array5 = {1, 2, 3, 4, 5, 6, 8, 9};
-//	    assertEquals((4+5)/2.0, median(array4));
-//	}
+
 }

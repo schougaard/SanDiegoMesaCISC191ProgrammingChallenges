3764265993f45ee9dca9877581333914f86477ea
domdavid <domdavid@localhost>
2023-08-17 17:52:35 -0700

    Added instruction to update GUI button by uncommenting a line of code to test methods via the GUI

--- CISC191ArrayChallengeMiddleDeveloper/src/PictureTester.java
+++ CISC191ArrayChallengeMiddleDeveloper/src/PictureTester.java
@@ -15,6 +15,26 @@ public class PictureTester
   public static void main(String[] args)
   {
 	  Picture beach = new Picture("dragonFly.PNG");
+	  
+	  
+	  /**
+	   * _____                _   __  __        _____  _                     
+		 |  __ \              | | |  \/  |      |  __ \| |                    
+		 | |__) |___  __ _  __| | | \  / | ___  | |__) | | ___  __ _ ___  ___ 
+		 |  _  // _ \/ _` |/ _` | | |\/| |/ _ \ |  ___/| |/ _ \/ _` / __|/ _ \
+		 | | \ \  __/ (_| | (_| | | |  | |  __/ | |    | |  __/ (_| \__ \  __/
+		 |_|  \_\___|\__,_|\__,_| |_|  |_|\___| |_|    |_|\___|\__,_|___/\___|
+	   * 
+	   * Instructions: 
+	   * As you complete a given filter, you must enable the button in the GUI under PictureExplorer.java. 
+	   * 
+	   * For example, look for the commented out line shown below in  PictureExplorer.java. Uncomment it so that it correctly
+	   * invokes your written blackAndWhite filter method. 
+	   *          // Logic.blackAndWhite(picture);
+	   * 
+	   * 
+	   */
+	  
 	  beach.explore();
 	  beach.explore();
 	  

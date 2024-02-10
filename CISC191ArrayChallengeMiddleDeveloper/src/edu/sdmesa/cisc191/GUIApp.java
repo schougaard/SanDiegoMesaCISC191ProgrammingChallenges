e09cbab2ed78fe0bc1f4781057176429d5989a81
allanschougaard <allanschougaard@192.168.1.68>
2024-02-10 13:43:07 -0800

    Added app in CISC191 package for ease of use.

---
+++ CISC191ArrayChallengeMiddleDeveloper/src/edu/sdmesa/cisc191/GUIApp.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package edu.sdmesa.cisc191;
+
+import edu.gatech.cc.PictureMain;
+
+/**
+ * Lead Author(s):
+ * 
+ * @author Allan Schougaard
+ * 
+ * Other contributors:
+ * 
+ *
+ * 
+ * References:
+ * @see Morelli, R., & Walde, R. (2016). Java, Java, Java: Object-Oriented
+ *      Problem Solving.
+ *      https://open.umn.edu/opentextbooks/textbooks/java-java-java-object-oriented-problem-solving
+ * 
+ * Version/date 1.0
+ * 
+ * Responsibilities of class:
+ * Run the application in a window
+ * 
+ */
+////////////////////////////////////////////////////////////////
+// *** Run this to start the application running in a window ***
+////////////////////////////////////////////////////////////////
+public class GUIApp 
+{
+	public static void main(String[] args)
+	{
+		PictureMain.main(args);
+	}
+}

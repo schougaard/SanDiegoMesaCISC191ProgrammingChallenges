b54c895532850d67c516d6d9fbe866085ab0d2da
Allan Schougaard <allanschougaard@yahoo.com>
2026-03-01 15:31:57 -0800

    Changed some methods to double to avoid typecasts.

--- CISC191M1Challenges/src/edu/sdmesa/cisc191/M1Challenge.java
+++ CISC191M1Challenges/src/edu/sdmesa/cisc191/M1Challenge.java
@@ -83,28 +83,6 @@ public class M1Challenge
 		// TODO: change this method to make the tests pass
 		return -99999;
 	}
-
-	/**
-	 * Purpose: 
-	 * @param array0
-	 * @return
-	 */
-	public static double sum(double[] array)
-	{
-		double sum = 0;
-		for(double d: array)
-		{
-			sum += d;
-		}
-		// TODO Auto-generated method stub
-		//return 1;
-		return sum;
-	}
-	
-	public static double average(double[] array)
-	{
-		return sum(array)/array.length;
-	}
 	
 	// Use this template for the other methods
 	/**
@@ -115,6 +93,7 @@ public class M1Challenge
 	 */
 //	public static ??? ???(???[] array)
 //	{
+//      // What is going on here?
 //		return ???;
 //	}
 
--- CISC191M1Challenges/src/edu/sdmesa/cisc191/TestM1Challenge.java
+++ CISC191M1Challenges/src/edu/sdmesa/cisc191/TestM1Challenge.java
@@ -180,41 +180,40 @@ class TestM1Challenge
 //		assertEquals(1, min(array6));
 //	}
 //
-	@Test
-	void testSum()
-	{
-		double[] array0 = {};
-		assertEquals(0, sum(array0));
-		
-		double[] array1 = {1};
-		assertEquals(1, sum(array1));
-		
-		double[] array2 = {1.1, 2.2, 3.3};
-		assertEquals(1.1+2.2+3.3, sum(array2));
-
-		double[] array3 = {-1, -2, 3};
-		assertEquals(-1+(-2)+3, sum(array3));
-
-		double[] array4 = {-1, -2, -3};
-		assertEquals(-1+(-2)+(-3), sum(array4));
-	}
+//	@Test
+//	void testSum()
+//	{
+//		double[] array0 = {};
+//		assertEquals(0, sum(array0));
+//		
+//		double[] array1 = {1};
+//		assertEquals(1, sum(array1));
+//		
+//		double[] array2 = {1.1, 2.2, 3.3};
+//		assertEquals(1.1+2.2+3.3, sum(array2));
 //
-	@Test
-	void testAverage()
-	{
-		double[] array1 = {17};
-		assertEquals(17.0, average(array1));
-		
-              // hint: can you use sum?
-		double[] array2 = {2, 3};
-		assertEquals((2+3)/2.0, average(array2));
-
-		double[] array3 = {-1, -2, 3};
-		assertEquals((-1+(-2)+3)/3.0, average(array3));
-
-		double[] array4 = {-1, -2, -3};
-		assertEquals((-1+(-2)+(-3))/3.0, average(array4));
-	}
-
+//		double[] array3 = {-1, -2, 3};
+//		assertEquals(-1+(-2)+3, sum(array3));
+//
+//		double[] array4 = {-1, -2, -3};
+//		assertEquals(-1+(-2)+(-3), sum(array4));
+//	}
+//
+//	@Test
+//	void testAverage()
+//	{
+//		double[] array1 = {17};
+//		assertEquals(17.0, average(array1));
+//		
+//        // hint: can you use sum?
+//		double[] array2 = {2, 3};
+//		assertEquals((2+3)/2.0, average(array2));
+//
+//		double[] array3 = {-1, -2, 3};
+//		assertEquals((-1+(-2)+3)/3.0, average(array3));
+//
+//		double[] array4 = {-1, -2, -3};
+//		assertEquals((-1+(-2)+(-3))/3.0, average(array4));
+//	}
 
 }
